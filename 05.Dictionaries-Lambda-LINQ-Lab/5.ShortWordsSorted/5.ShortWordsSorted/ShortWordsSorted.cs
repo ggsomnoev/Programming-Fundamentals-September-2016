21b80b45742b47e463a99b35dc9570f94b11ea27
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-02 09:43:07 +0200

    added few ex

---
+++ 05.Dictionaries-Lambda-LINQ-Lab/5.ShortWordsSorted/5.ShortWordsSorted/ShortWordsSorted.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.ShortWordsSorted
+{
+    class ShortWordsSorted
+    {
+        static void Main(string[] args)
+        {
+            char[] sep = new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '\"', '\'', '\\', '/', '!', '?', ' ' };
+            string[] words = Console.ReadLine().ToLower().Split(sep).Where(x => x != "" && x.Length < 5).OrderBy(x => x).Distinct().ToArray();
+            Console.WriteLine(string.Join(", ", words));               
+        }
+    }
+}
---
+++ 05.Dictionaries-Lambda-LINQ-Lab/6.FoldAndSum/6.FoldAndSum/FoldAndSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.FoldAndSum
+{
+    class FoldAndSum
+    {
+        static void Main(string[] args)
+        {            
+            int[] values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int part = values.Length / 4;
+            int[] row1 = values.Take(part).Reverse().Concat(values.Reverse().Take(part)).ToArray();
+            int[] row2 = values.Skip(part).Take(2 * part).ToArray();
+            int[] sum = row1.Select((x, index) => x + row2[index]).ToArray();
+            Console.WriteLine("{0} +\n{1} =\n{2}", string.Join(" ", row1), string.Join(" ", row2), string.Join(" ", sum));
+        }
+    }
+}
