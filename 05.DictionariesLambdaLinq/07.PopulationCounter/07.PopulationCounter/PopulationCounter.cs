3b793e37918ada4d9e388a302b526523d26254b8
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-15 18:28:41 +0200

    a

---
+++ 05.DictionariesLambdaLinq/07.PopulationCounter/07.PopulationCounter/PopulationCounter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.PopulationCounter
+{
+    class PopulationCounter
+    {
+        static void Main(string[] args)
+        {
+            string country, city;
+            int ppl;
+            string[] parts;
+            Dictionary<string, int> summed = new Dictionary<string, int>();
+            Dictionary<string, Dictionary<string, int>> pop = new Dictionary<string, Dictionary<string, int>>();
+            string inputLine = Console.ReadLine();
+            while (inputLine != "report" && inputLine != "")
+            {
+                parts = inputLine.Split('|');
+                country = parts[1];
+                city = parts[0];
+                ppl = int.Parse(parts[2]);
+
+                if (pop.ContainsKey(country))
+                {
+                    if (pop[country].ContainsKey(city))
+                    {
+                        pop[country][city] += ppl;
+                    }
+                    else
+                    {
+                        pop[country][city] = ppl;
+                    }
+                }
+                else
+                {
+                    pop[country] = new Dictionary<string, int>();
+                    pop[country][city] = ppl;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var item in pop)
+            {
+                summed[item.Key] = item.Value.Values.Sum();
+            }
+            var test = from entr in summed orderby entr.Value descending select entr;
+
+            foreach (var item in test)
+            {
+                Console.WriteLine("{0} (total population: {1})", item.Key, item.Value);
+                foreach (var outer in pop)
+                {
+                    foreach (var inner in outer.Value)
+                    {
+                        Console.WriteLine("=>{0}: {1}", inner.Key, inner.Value);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ 05.DictionariesLambdaLinq/08.LogsAggregator/08.LogsAggregator/LogsAggregator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.LogsAggregator
+{
+    class LogsAggregator
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string ip, name;
+            int value;
+            string[] inputLine;
+            SortedDictionary<string, SortedDictionary<string, int>> userBase = new SortedDictionary<string, SortedDictionary<string, int>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                inputLine = Console.ReadLine().Split(' ');
+                ip = inputLine[0];
+                name = inputLine[1];
+                value = Convert.ToInt32(inputLine[2]);
+
+                if (userBase.ContainsKey(name))
+                {
+                    if (userBase[name].ContainsKey(ip))
+                    {
+                        userBase[name][ip] += value;
+                    }
+                    else
+                    {
+                        userBase[name][ip] = value;
+                    }
+                }
+                else
+                {
+                    userBase[name] = new SortedDictionary<string, int>();
+                    userBase[name][ip] = value;
+                }
+            }
+            foreach (var item in userBase)
+            {
+                var sum = item.Value.Values.Sum();
+
+                Console.WriteLine("{0}: {1} [{2}]", item.Key, sum, string.Join(", ", item.Value.Keys));
+            }
+
+        }
+    }
+}
