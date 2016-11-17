b94c60cbb88de924e973c269889856b7412ecd71
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-17 16:01:35 +0200

    eddited

--- 05.DictionariesLambdaLinq/09.LegendaryFarming/09.LegendaryFarming/LegendaryFarming.cs
+++ 05.DictionariesLambdaLinq/09.LegendaryFarming/09.LegendaryFarming/LegendaryFarming.cs
@@ -10,84 +10,82 @@ namespace _09.LegendaryFarming
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> items = new Dictionary<string, int>();
-            Dictionary<string, int> sortedLegendaries = new Dictionary<string, int>();
+            Dictionary<string, int> junk = new Dictionary<string, int>();
+            Dictionary<string, int> keyMaterials = new Dictionary<string, int>();
 
             int i = 1;
             string[] input = Console.ReadLine().ToLower().Split();
             while (i < input.Length)
-            { 
-                if (items.ContainsKey(input[i]))
+            {
+                if (keyMaterials.ContainsKey(input[i]) && (input[i] == "shards" || input[i] == "fragments" || input[i] == "motes"))
                 {
-                    items[input[i]] += int.Parse(input[i - 1]);
+                    keyMaterials[input[i]] += int.Parse(input[i - 1]);
                 }
-                else
+                else if(input[i] == "shards" || input[i] == "fragments" || input[i] == "motes")
                 {
-                    items[input[i]] = int.Parse(input[i - 1]);
+                    keyMaterials[input[i]] = int.Parse(input[i - 1]);
+                }
+                else if(!junk.ContainsKey(input[i]))
+                {
+                    junk[input[i]] = int.Parse(input[i - 1]);
                 }
 
                 i += 2;
             }
-            if (items.ContainsKey("shards"))
+
+            Container(keyMaterials);
+
+            junk = junk.OrderBy(x => x.Key).ToDictionary(p => p.Key, p => p.Value);
+            keyMaterials = keyMaterials.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(p => p.Key, p => p.Value);
+
+            foreach (var sortedMats in keyMaterials)
             {
-                if (items["shards"] >= 250)
+                Console.WriteLine("{0}: {1}", sortedMats.Key, sortedMats.Value);
+            }
+
+            foreach (var item in junk)
+            {
+                Console.WriteLine("{0}: {1}", item.Key, item.Value);
+            }       
+        }
+        static void Container(Dictionary<string, int> junk) {
+            if (junk.ContainsKey("shards"))
+            {
+                if (junk["shards"] >= 250)
                 {
-                    items["shards"] -= 250;
+                    junk["shards"] -= 250;
                     Console.WriteLine("Shadowmourne obtained!");
                 }
             }
             else
             {
-                items["shards"] = 0;
+                junk["shards"] = 0;
             }
 
-            if (items.ContainsKey("fragments"))
+            if (junk.ContainsKey("fragments"))
             {
-                if (items["fragments"] >= 250)
+                if (junk["fragments"] >= 250)
                 {
-                    items["fragments"] -= 250;
+                    junk["fragments"] -= 250;
                     Console.WriteLine("Valanyr obtained!");
                 }
             }
             else
             {
-                items["fragments"] = 0;
+                junk["fragments"] = 0;
             }
 
-            if (items.ContainsKey("motes"))
+            if (junk.ContainsKey("motes"))
             {
-                if (items["motes"] >= 250)
+                if (junk["motes"] >= 250)
                 {
-                    items["motes"] -= 250;
+                    junk["motes"] -= 250;
                     Console.WriteLine("Dragonwrath obtained!");
                 }
             }
             else
             {
-                items["motes"] = 0;
-            }
-
-            sortedLegendaries["fragments"] = items["fragments"];
-            sortedLegendaries["shards"] = items["shards"];
-            sortedLegendaries["motes"] = items["motes"];
-
-
-            items = items.OrderBy(x => x.Value).ToDictionary(p => p.Key, p => p.Value);
-            sortedLegendaries = sortedLegendaries.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(p => p.Key, p => p.Value);
-
-            //Console.WriteLine("{0}", string.Join(", ", items));
-            foreach (var sortedMats in sortedLegendaries)
-            {
-                Console.WriteLine("{0}: {1}", sortedMats.Key, sortedMats.Value);
-            }
-
-            foreach (var item in items)
-            {
-                if (item.Key != "motes" && item.Key != "fragments" && item.Key != "shards")
-                {
-                    Console.WriteLine("{0}: {1}", item.Key, item.Value);
-                }
-                
+                junk["motes"] = 0;
             }
         }
     }
