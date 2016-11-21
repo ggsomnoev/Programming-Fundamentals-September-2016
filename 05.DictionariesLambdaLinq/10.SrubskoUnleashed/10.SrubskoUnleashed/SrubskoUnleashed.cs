fbe724f7414800d97bc025880c437e208e3971cf
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-21 17:01:25 +0200

    added and editted

---
+++ 05.DictionariesLambdaLinq/10.SrubskoUnleashed/10.SrubskoUnleashed/SrubskoUnleashed.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.SrubskoUnleashed
+{
+    class SrubskoUnleashed
+    {
+        static void Main(string[] args)
+        {
+            string[] parts;
+            string performer, venue, inputLine = Console.ReadLine();
+            int money;
+            Dictionary<string, Dictionary<string, int>> records = new Dictionary<string, Dictionary<string, int>>();
+            
+            while (inputLine.ToLower() != "end")
+            {
+                string[] input = inputLine.Split('@');
+
+                if (InputValidation(input[0], input[1]))
+                {
+                    parts = input[1].Split(' ');
+
+                    performer = input[0].TrimEnd(' ');
+                    venue = string.Join(" ", parts.Reverse().Skip(2).Take(parts.Length - 2).Reverse());
+                    money = int.Parse(parts[parts.Length - 1]) * int.Parse(parts[parts.Length - 2]);
+
+                    if (records.ContainsKey(venue))
+                    {
+                        if (records[venue].ContainsKey(performer))
+                        {
+                            records[venue][performer] += money;
+                        }
+                        else
+                        {
+                            records[venue][performer] = money;
+                        }
+                    }
+                    else
+                    {
+                        records[venue] = new Dictionary<string, int>();
+                        records[venue][performer] = money;
+                    }
+                    //Console.WriteLine("{0}, {1}, {2}", performer, venue, money);      
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                inputLine = Console.ReadLine();
+            }
+
+            //records = records.OrderByDescending(x => x.Value.Values).ToDictionary(p => p.Key, p => p.Value);
+
+            foreach (var outer in records)
+            {
+                Console.WriteLine(outer.Key);
+                foreach (var inner in outer.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine("#  {0} -> {1}", inner.Key, inner.Value);
+                }
+            }
+        }
+        static bool InputValidation(string a, string b)
+        {
+            string[] parts = b.Split(' ');
+            int container;
+
+            if (a.EndsWith(" ") && 
+                parts.Length >= 3 && (
+                int.TryParse(parts[parts.Length - 1], out container) &&
+                int.TryParse(parts[parts.Length - 2], out container)
+                ))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 05.DictionariesLambdaLinq/11.DragonsArmy/11.DragonsArmy/DragonsArmy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.DragonsArmy
+{
+    class DragonsArmy
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string[] value;
+            Dictionary<string, Dictionary<string, List<string>>> data = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < number; i++)
+            {
+                value = Console.ReadLine().Split(' ');
+                if (value[2] == "null")
+                {
+                    value[2] = "45";
+                }
+                if (value[3] == "null")
+                {
+                    value[3] = "250";
+                }
+                if (value[4] == "null")
+                {
+                    value[4] = "10";
+                }
+
+                if (data.ContainsKey(value[0]))
+                {
+                    data[value[0]][value[1]] = new List<string>();
+                    data[value[0]][value[1]].Add(value[2]);
+                    data[value[0]][value[1]].Add(value[3]);
+                    data[value[0]][value[1]].Add(value[4]);
+                }
+                else
+                {
+                    data[value[0]] = new Dictionary<string, List<string>>();
+                    data[value[0]][value[1]] = new List<string>();
+
+                    data[value[0]][value[1]].Add(value[2]);
+                    data[value[0]][value[1]].Add(value[3]);
+                    data[value[0]][value[1]].Add(value[4]);
+                }
+            }
+
+            foreach (var item in data)
+            {
+                Console.WriteLine("{0}::({1:F2}/{2:F2}/{3:F2})", item.Key, item.Value.Values.ToArray().Select(x => int.Parse(x[0])).Average(), item.Value.Values.ToArray().Select(x => int.Parse(x[1])).Average(), item.Value.Values.ToArray().Select(x => int.Parse(x[2])).Average());
+                foreach (var inner in item.Value.OrderBy(x => x.Key))
+                {
+                    Console.WriteLine("-{0} -> damage: {1}, health: {2}, armor: {3}", inner.Key, inner.Value[0], inner.Value[1], inner.Value[2]);
+                }
+            }
+        }
+    }
+}
