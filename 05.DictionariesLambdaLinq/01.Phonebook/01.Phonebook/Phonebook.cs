713b5b864c39cf4936b2e962ced92efbbdd5fc27
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-22 09:22:23 +0200

    added new ex

---
+++ 05.DictionariesLambdaLinq/01.Phonebook/01.Phonebook/Phonebook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Phonebook
+{
+    class Phonebook
+    {
+        static void Main(string[] args)
+        {
+            string[] parts;
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            string command, name, phone = "***", inputLine = Console.ReadLine();
+            while (inputLine != "END") {
+                parts = inputLine.Split(' ');
+                command = parts[0];
+                name = parts[1];
+                if(parts.Length > 2) {
+                    phone = parts[2];
+                }                
+                if (command == "A")
+                {
+                    phonebook[name] = phone;
+                }
+                else if (command == "S")
+                {
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine("{0} => {1}", name, phonebook[name]);
+                    }
+                    else {
+                        Console.WriteLine("Contact {0} doesn't exist in the phonebook", name);
+                    }
+                }
+                else {
+                    Console.WriteLine("Invalid Command");
+                } 
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 05.DictionariesLambdaLinq/02.PhonebookUpgrade/02.PhonebookUpgrade/PhonebookUpgrade.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.PhonebookUpgrade
+{
+    class PhonebookUpgrade
+    {
+        static void Main(string[] args)
+        {
+            string[] parts;
+            SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+            string command, name = "no name", phone = "no number", inputLine = Console.ReadLine();
+            while (inputLine != "END")
+            {
+                parts = inputLine.Split(' ');
+                command = parts[0];
+                if (parts.Length > 1)
+                {
+                    name = parts[1];
+                }
+                if (parts.Length > 2)
+                {
+                    phone = parts[2];
+                }
+                if (command == "A")
+                {
+                    phonebook[name] = phone;
+                }
+                else if (command == "S")
+                {
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine("{0} => {1}", name, phonebook[name]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact {0} doesn't exist in the phonebook", name);
+                    }
+                }
+                else if (command == "ListAll")
+                {
+                    //var result = from pair in phonebook
+                    //            orderby pair.Key ascending
+                    //            select pair;
+                    foreach (var item in phonebook)
+                    {
+                        Console.WriteLine("{0} => {1}", item.Key, item.Value);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command");
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
