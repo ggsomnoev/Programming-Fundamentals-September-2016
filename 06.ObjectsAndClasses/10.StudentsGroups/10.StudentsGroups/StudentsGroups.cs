c80e55f0b2b365a894cb309ecdab244b050475bd
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-23 18:22:50 +0200

    added new ex

---
+++ 06.ObjectsAndClasses/10.StudentsGroups/10.StudentsGroups/StudentsGroups.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.StudentsGroups
+{
+    class StudentsGroups
+    {
+        static void Main(string[] args)
+        {
+            string[] parts;
+            List<Town> groups = new List<Town>();
+            Town town = new Town();
+            Student student;
+
+            string inputLine = Console.ReadLine();
+            while (inputLine.ToLower() != "end")
+            {
+                parts = inputLine.Split(new char[] {' ', '|'}).Where(x => x != "").ToArray();
+
+                //Console.WriteLine("{0}", string.Join(", ", parts));
+                if (parts[parts.Length - 3] == "=>")
+                {
+                    town = new Town();
+                    town.Students = new List<Student>();
+                    town.Name = parts[0];
+                    town.SeatsCount = int.Parse(parts[parts.Length - 2]);
+                    if (parts[1] != "=>")
+                    {
+                        town.Name = string.Concat(parts[0], " ", parts[1]);
+                    }
+                }
+                else
+                {
+                    student = new Student();
+                    student.Name = string.Concat(parts[0], " ", parts[1]);
+                    student.Email = parts[2];
+                    student.Date = DateTime.Parse(parts[3]);
+                    town.Students.Add(student);                    
+                }
+                inputLine = Console.ReadLine();
+
+                if (parts[parts.Length - 3] == "=>") {
+                    groups.Add(town);
+                }
+            }
+
+            foreach (var item in groups)
+            {
+                int counter = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(item.Students.Count) / item.SeatsCount));
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
---
+++ 06.ObjectsAndClasses/10.StudentsGroups/10.StudentsGroups/Town.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.StudentsGroups
+{
+    class Town
+    {
+        public string Name { set; get; }
+        public int SeatsCount { set; get; }
+        public List<Student> Students { set; get; }
+    }
+}
