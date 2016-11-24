3671b7ab60961be6e7f2aa7e28ed58a528e6f60a
ggsomnoev <georgisomnoev@yahoo.com>
2016-11-24 16:45:30 +0200

    q

--- 06.ObjectsAndClasses/10.StudentsGroups/10.StudentsGroups/StudentsGroups.cs
+++ 06.ObjectsAndClasses/10.StudentsGroups/10.StudentsGroups/StudentsGroups.cs
@@ -18,9 +18,8 @@ namespace _10.StudentsGroups
             string inputLine = Console.ReadLine();
             while (inputLine.ToLower() != "end")
             {
-                parts = inputLine.Split(new char[] {' ', '|'}).Where(x => x != "").ToArray();
+                parts = inputLine.Split(new char[] { ' ', '|' }).Where(x => x != "").ToArray();
 
-                //Console.WriteLine("{0}", string.Join(", ", parts));
                 if (parts[parts.Length - 3] == "=>")
                 {
                     town = new Town();
@@ -38,19 +37,44 @@ namespace _10.StudentsGroups
                     student.Name = string.Concat(parts[0], " ", parts[1]);
                     student.Email = parts[2];
                     student.Date = DateTime.Parse(parts[3]);
-                    town.Students.Add(student);                    
+                    town.Students.Add(student);
                 }
                 inputLine = Console.ReadLine();
 
-                if (parts[parts.Length - 3] == "=>") {
+                if (parts[parts.Length - 3] == "=>")
+                {
                     groups.Add(town);
                 }
             }
 
-            foreach (var item in groups)
+            foreach (var outer in groups)
             {
-                int counter = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(item.Students.Count) / item.SeatsCount));
-                Console.WriteLine(counter);
+                Console.WriteLine("{0}:", outer.Name);
+                for (int i = 0; i < outer.Students.Count; i++)
+                {
+                    Console.WriteLine("{0}, ".TrimEnd(new char[] { ' ', ',' }), outer.Students[i].Email);
+                }
+            }
+
+            double totalGroups = groups.Select(x => (Math.Ceiling(Convert.ToDouble(x.Students.Count) / x.SeatsCount))).Sum();
+            Console.WriteLine("Created {0} groups in {1} towns:", totalGroups, groups.Count);
+
+            foreach (var outer in groups)
+            {
+                int counter = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(outer.Students.Count) / outer.SeatsCount));
+                int n = 0;
+                for (int i = 0; i < counter; i++)
+                {
+                    if (n + outer.SeatsCount >= outer.Students.Count)
+                    {
+                        Console.WriteLine("{0} => {1}", outer.Name, string.Join(", ", outer.Students.Skip(n).Take(outer.Students.Count - n).Select(x => x.Email)));
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} => {1}", outer.Name, string.Join(", ", outer.Students.Skip(n).Take(outer.SeatsCount).Select(x => x.Email)));
+                        n = outer.SeatsCount;
+                    }
+                }
             }
         }
     }
